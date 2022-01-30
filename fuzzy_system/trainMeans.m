dd07fbd3ff3fa04de7325750192b52b20582666e
Filip <filipsurma96@gmail.com>
2022-01-30 18:30:13 +0100

    gradient decent for fuzzy systems works fine

--- fuzzy_system/trainMeans.m
+++ fuzzy_system/trainMeans.m
@@ -1,11 +1,10 @@
-function fis=trainVaraince(fis,oldFis,u,inputs,groundTruth,fis_outputs,learningRate)
+function fis=trainMeans(fis,oldFis,u,inputs,groundTruth,fis_outputs,learningRate)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 errors=groundTruth-fis_outputs;
 numberOfInputs=length(fis.Inputs);
 numberOfOuputs=length(errors);
 numberOfRules=length(fis.Rules);
-
 
 for i=1:numberOfInputs
     numberOfMemberships=length(fis.Inputs(i).MembershipFunctions);
@@ -17,12 +16,12 @@ for i=1:numberOfInputs
             insideHelper=0;
             for k=1:numberOfRules
                 insideHelper=insideHelper+(oldFis.Outputs(1).MembershipFunctions(k).Parameters-fis_outputs(j))*...
-                    memberships(k)*(inputs(j)-oldFis.Inputs(i).MembershipFunctions(m).Parameters(2))/oldFis.Inputs(i).MembershipFunctions(m).Parameters(1)^2;
+                    memberships(k)*(inputs(j)-oldFis.Inputs(i).MembershipFunctions(m).Parameters(2))^2/oldFis.Inputs(i).MembershipFunctions(m).Parameters(1)^3;
             end
             
             helperValue=helperValue+errors(j)/sum(memberships)*insideHelper*2;
         end
-        fis.Inputs(i).MembershipFunctions(m).Parameters(1)=...
-            fis.Inputs(i).MembershipFunctions(m).Parameters(1)+learningRate*helperValue;
+        fis.Inputs(i).MembershipFunctions(m).Parameters(2)=...
+            fis.Inputs(i).MembershipFunctions(m).Parameters(2)+learningRate*helperValue;
     end
 end
